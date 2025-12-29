f134b90b31427e6f6680937e296c723bc59da38f
manicharan18 <manicharankancharla55@gmail.com>
2025-12-29 21:29:47 +0530

    Q1.py

---
+++ Q1.py
@@ -0,0 +1,12 @@
+# Variables describing myself
+age = 18                  
+height = 5.6          
+name = "Mani Charan"         
+are_you_student = true
+
+# Nicely formatted output
+print("=== About Me ===")
+print(f"Name: {name}")
+print(f"Age: {age} years old")
+print(f"Height: {height} meters")
+print(f"Are you a student? {are_you_student}")
