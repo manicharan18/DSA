e1ab9ae0b6dc814a38f2a26a1e4cde5ba771016c
manicharan18 <manicharankancharla55@gmail.com>
2026-01-03 23:56:44 +0530

    Q19.py

---
+++ Q19.py
@@ -0,0 +1,18 @@
+grades = [85, 92, 78, 65, 88, 91, 73, 89, 55, 94]
+
+A = B = C = below = 0
+
+for g in grades:
+    if g >= 90:
+        A += 1
+    elif g >= 80:
+        B += 1
+    elif g >= 70:
+        C += 1
+    else:
+        below += 1
+
+print("A:", A)
+print("B:", B)
+print("C:", C)
+print("Below 70:", below)
